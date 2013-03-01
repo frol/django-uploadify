ec27381a3609d0faa33f4ccbb5bec00517cbef00
frol <frolvlad@gmail.com>
2013-03-01 12:36:27 +0200

    add optional jinja2 support

--- uploadify/templatetags/uploadify_tags.py
+++ uploadify/templatetags/uploadify_tags.py
@@ -1,21 +1,41 @@
-from django import template
-from django.template import resolve_variable
+from django.conf import settings as django_settings
+from django.template import Node, resolve_variable
+from django.template.loader import render_to_string
 from django.utils import simplejson
+
+if 'coffin' in django_settings.INSTALLED_APPS:
+    from coffin.template import Library
+    is_coffin = True
+else:
+    from django.template import Library
+    is_coffin = False
 
 from uploadify import settings
 
-register = template.Library()
+register = Library()
 
 # -----------------------------------------------------------------------------
 #   multi_file_upload
 # -----------------------------------------------------------------------------
-class MultiFileUpload(template.Node):
+class MultiFileUpload(Node):
     def __init__(self, sender='"uploadify"', unique_id=None, data=None, **kwargs):
         self.sender = sender
         self.unique_id = unique_id
         self.data = data or {}
         self.options = kwargs
-       
+
+    def build_context(self, unique_id, data, options, auto):
+        return {
+            'uploadify_query': unique_id,
+            'uploadify_data': simplejson.dumps(data)[1:-1],
+            'uploadify_path': settings.UPLOADIFY_PATH,
+            'uploadify_version': settings.UPLOADIFY_VERSION,
+            'uploadify_options': ','.join(("'%s': '%s'" % (item[0], item[1])
+                                            for item in options.items())),
+            'uploadify_filename': options['fileDataName'],
+            'uploadify_auto': auto,
+        }
+
     def render(self, context):
         if self.unique_id is not None:
             unique_id = "?unique_id=%s" % str(resolve_variable(self.unique_id, context))
@@ -25,8 +45,6 @@ class MultiFileUpload(template.Node):
         options = {'fileDataName': 'Filedata'}
         for key, value in self.options.items():
             options[key] = resolve_variable(value, context)
-        js_options = ",".join(map(lambda item: "'%s': '%s'" % (item[0], item[1]),
-                                  options.items()))
 
         auto = options.get('auto', False)
         
@@ -37,17 +55,9 @@ class MultiFileUpload(template.Node):
         for key, value in self.data.items():
             data[key] = resolve_variable(value, context)
 
-        context.update({
-            'uploadify_query': unique_id,
-            'uploadify_data': simplejson.dumps(data)[1:-1],
-            'uploadify_path': settings.UPLOADIFY_PATH,
-            'uploadify_options': js_options,
-            'uploadify_filename': options['fileDataName'],
-            'uploadify_auto': auto,
-        })
+        context.update(self.build_context(unique_id, data, options, auto))
 
-        t = template.loader.get_template('uploadify/multi_file_upload.html')
-        return t.render(context)
+        return render_to_string('uploadify/multi_file_upload.html', context)
 
 
 @register.tag
@@ -72,3 +82,36 @@ def multi_file_upload(parser, token):
         options[name] = val
 
     return MultiFileUpload(**options)
+
+
+if is_coffin:
+    from coffin.template.loader import render_to_string as coffin_render_to_string
+    from jinja2 import Markup
+
+    @register.object
+    def multi_file_upload(request, sender='uploadify', unique_id=None, data=None, **options):
+        """
+        Jinja2 tag.
+
+        Displays a Flash-based interface for uploading multiple files.
+        For each POST request (after file upload) send GET query with `unique_id`.
+
+        {{ multi_file_upload(request, sender='SomeThing', fileDataName='Filename') }}
+
+        For all options see http://www.uploadify.com/documentation/
+        """
+        if 'fileDataName' not in options:
+            options['fileDataName'] = 'Filedata'
+
+        auto = options.get('auto', False)
+        
+        _data = {
+            'fileDataName': options['fileDataName'],
+            'sender': sender,
+        }
+        if data:
+            _data.update(data)
+
+        multi_file_upload = MultiFileUpload(sender, unique_id, data, **options)
+        return Markup(coffin_render_to_string('uploadify/multi_file_upload.html',
+            dict(multi_file_upload.build_context(unique_id, _data, options, auto), request=request)))
