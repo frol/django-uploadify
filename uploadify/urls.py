ee6a954e940b2f9bb9a8a83e32a06462095d5a59
frol <frolvlad@gmail.com>
2013-04-29 13:50:12 +0300

    fix urls import for Django 1.6 support

--- uploadify/urls.py
+++ uploadify/urls.py
@@ -1,4 +1,8 @@
-from django.conf.urls.defaults import *
+try:
+    # Django 1.6.0
+    from django.conf.urls import patterns, url
+except ImportError:
+    from django.conf.urls.defaults import patterns, url
 from uploadify.views import *
 
 urlpatterns = patterns('',
